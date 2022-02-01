fa62cc59bd7d3e59a8c82b469dc266c44543a425
codeninjah <codeninjahblog@gmail.com>
2022-02-01 12:03:04 +0100

    First commit

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="library">
+    <p>{{ myArray[0] }}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      myArray: [
+        { name: "LOTR", author: "J.R.R. Tolkien" },
+        { name: "Clean Code", author: "Bob Martin" },
+        { name: "Coding JS", author: "David L" },
+      ],
+    };
+  },
+};
+</script>
+
+
+<style scoped >
+
+</style>
