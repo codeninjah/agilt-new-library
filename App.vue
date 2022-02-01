99382cd255d5d83270471566f6e7e8e6dcb15744
codeninjah <codeninjahblog@gmail.com>
2022-02-01 13:36:47 +0100

    Progress

--- App.vue
+++ App.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="library">
-    <p>{{ myArray[0] }}</p>
+    <div class="header">
+      <h1>Leifs' Kool Skool Library</h1>
+      <p>A brief collection of our books</p>
+    </div>
+    <ul v-if="available == false" v-bind:class="av">{{myArray[0].name}}</ul>
+    <ul>
+      <li v-for="bok in myArray" v-bind:key="bok.name" >
+        {{ bok.name }} {{ bok.author }} {{ bok.available }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,10 +18,11 @@
 export default {
   data() {
     return {
+        available: '', //är detta true, visas alla böcker
       myArray: [
-        { name: "LOTR", author: "J.R.R. Tolkien" },
-        { name: "Clean Code", author: "Bob Martin" },
-        { name: "Coding JS", author: "David L" },
+        { name: "LOTR", author: "J.R.R. Tolkien", available: false },
+        { name: "Clean Code", author: "Bob Martin", available: true },
+        { name: "Coding JS", author: "David L", available: true },
       ],
     };
   },
@@ -21,5 +31,29 @@ export default {
 
 
 <style scoped >
+body {
+  background-color: #c4c4c4;
+}
+.header {
+  background-color: hotpink;
+  height: 10vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.library {
+  background-color: peachpuff;
+  margin-left: auto;
+  margin-right: auto;
+  width: 70%;
+}
 
+.un {
+    color: red;
+}
+
+.av { 
+    color: green;
+}
 </style>
